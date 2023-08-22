197167b63f1ee9a33eeb74c1825cd92f30668933
olhaart <artimnetco@gmail.com>
2023-08-22 17:51:29 -0500

    initial push

---
+++ src/test/java/mentoring_1/LambaTest.java
@@ -0,0 +1,22 @@
+package mentoring_1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LambaTest {
+    @Test
+    public void meow(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.lambdatest.com/selenium-playground/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.findElements(By.cssSelector(""));
+    }
+}
